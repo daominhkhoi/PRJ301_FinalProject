b348d77129be259a093ad2303c13bdfe8dbeeab6
hieuFPT <xuanhieubato@gmail.com>
2026-02-04 14:42:02 +0700

    Add files via upload

--- DeliverAcctAutoAlert/src/java/model/UserDTO.java
+++ DeliverAcctAutoAlert/src/java/model/UserDTO.java
@@ -1,13 +1,58 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package model;
 
-/**
- *
- * @author Admin
- */
 public class UserDTO {
+    private int userId;
+    private String userName;
+    private String password;
+    private String fullName;
+    private int roleId;
+
+    public UserDTO(int userId, String userName, String password, String fullName, int roleId) {
+        this.userId = userId;
+        this.userName = userName;
+        this.password = password;
+        this.fullName = fullName;
+        this.roleId = roleId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
     
 }
